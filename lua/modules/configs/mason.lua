a791e7067dc691676ca19b87ad5ea2b11fcdb48f
Ryan Barry <rbarry@mirantis.com>
2023-01-21 07:31:09 -0500

    Mason installation

--- lua/modules/configs/mason.lua
+++ lua/modules/configs/mason.lua
@@ -1,4 +1,4 @@
-require("mason").setup {
+local opts = {
    ui = {
       border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
       icons = {
@@ -7,4 +7,22 @@ require("mason").setup {
          package_uninstalled = "✗",
       },
    },
+   ensure_installed = {
+      "black",
+      "codespell",
+      "gopls",
+      "isort",
+      "mypy",
+      "shellcheck",
+      "shfmt",
+   },
 }
+require("mason").setup { opts }
+
+local mr = require "mason-registry"
+for _, tool in ipairs(opts.ensure_installed) do
+   local p = mr.get_package(tool)
+   if not p:is_installed() then
+      p:install()
+   end
+end
